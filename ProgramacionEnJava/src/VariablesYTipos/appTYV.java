87318055668d0074ce20490791ceea5cfc34cdb2
Adrian Velez <adrianvelez1995@gmail.com>
2017-12-08 13:50:27 -0600

    Mi segunda aplicacion para los tipos de datos

---
+++ ProgramacionEnJava/src/VariablesYTipos/appTYV.java
@@ -0,0 +1,26 @@
+package VariablesYTipos;
+
+public class appTYV {
+
+    public static void main(String[] args){//Estructura principal del proyecto
+
+        int entero;
+        boolean elige;
+        String soyUnaPalabra;
+        float soydecimal;
+        double soymaspreciso;
+        char palabracorta;
+
+        entero= 23;
+        elige=true;
+        soyUnaPalabra="Hola mundo";
+        soydecimal= 14.34f;
+        soymaspreciso=23.34;
+        palabracorta='a';
+
+        System.out.println(entero + soyUnaPalabra + soydecimal +soymaspreciso + soyUnaPalabra);
+
+
+    }
+
+}
