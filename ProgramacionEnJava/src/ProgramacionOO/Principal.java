a14d453ee065ae37943e0101b0411cd625383a37
Adrian Velez <adrianvelez1995@gmail.com>
2018-01-04 23:22:50 -0600

    Introduccion a las clase Abstractas

---
+++ ProgramacionEnJava/src/ProgramacionOO/Principal.java
@@ -0,0 +1,14 @@
+package ProgramacionOO;
+
+public class Principal {
+
+    public void main(String[] args){
+        Recipiente vasito= new Recipiente();
+
+        vasito.llenarRecipiente();
+
+
+
+    }
+
+}
