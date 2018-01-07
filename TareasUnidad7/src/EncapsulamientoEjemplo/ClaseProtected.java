c0dccfc193b0bd36c0e67b93661077691b031045
Adrian Velez <adrianvelez1995@gmail.com>
2018-01-06 21:08:48 -0600

    Encapsulacion de metodos y modificadores de acceso

---
+++ TareasUnidad7/src/EncapsulamientoEjemplo/ClaseProtected.java
@@ -0,0 +1,12 @@
+package EncapsulamientoEjemplo;
+
+public class ClaseProtected extends ModificadoresdeAcceso {
+
+    public void hijoHeredaProtected(){
+        accesoaMetodoProtegido("Este metodo es protegido y solo se puede usar en una clase heredada o en otro paquete heredado.");
+    }
+
+
+
+
+}
---
+++ TareasUnidad7/src/EncapsulamientoEjemplo/ClasedeAcceso.java
@@ -0,0 +1,30 @@
+package EncapsulamientoEjemplo;
+
+public class ClasedeAcceso {
+
+    private int entero1;
+    private String cadena1;
+    private boolean estado;
+
+
+    public int getEntero1() {
+        return entero1;
+    }
+    public void setEntero1(int entero1) {
+        this.entero1 = entero1;
+    }
+
+    public String getCadena1() {
+        return cadena1;
+    }
+    public void setCadena1(String cadena1) {
+        this.cadena1 = cadena1;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
